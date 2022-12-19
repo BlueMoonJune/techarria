f72591ba2dcab30fed37abc5e1b5a632a6d96933
Gwabba <gwabbawaba@gmail.com>
2022-12-18 18:19:22 -0800

    itemPlacer hates me

---
+++ Content/Tiles/ItemPlacer.cs
@@ -0,0 +1,54 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.Localization;
+
+namespace techarria.Content.Tiles
+{
+    internal class ItemPlacer : ModTile
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileFrameImportant[Type] = true;
+
+            AddMapEntry(Color.Blue, CreateMapEntryName());
+
+            DustType = DustID.Stone;
+            ItemDrop = ModContent.ItemType<Items.Placeables.ItemPlacer>();
+
+            HitSound = SoundID.Tink;
+        }
+
+        public override bool Slope(int i, int j)
+        {
+            Tile tile = Framing.GetTileSafely(i, j);
+            tile.TileFrameX = (short)((tile.TileFrameX + 16) % 64);
+            return false;
+        }
+
+        public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak)
+        {
+            return false;
+        }
+
+        public override void HitWire(int i, int j)
+        {
+            int xOff = 0;
+            int yOff = 0;
+            Tile tile = Framing.GetTileSafely(i, j);
+            if (tile.TileFrameX == 0) {
+                xOff = 1;
+            } else if (tile.TileFrameX == 16) {
+                yOff = -1;
+            } else if (tile.TileFrameX == 32) {
+                xOff = -1;
+            } else {
+                yOff = 1;
+            }
+            Main.LocalPlayer.PickTile(i + xOff, j + yOff, 40);
+        }
+    }
+}
