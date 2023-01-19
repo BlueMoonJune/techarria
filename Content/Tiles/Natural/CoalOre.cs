f70fd0b43c0b7e939bc42b9ce6a6aac6027844b2
Gwabba <gwabbawaba@gmail.com>
2023-01-18 21:58:23 -0800

    Coal ore spawns now, does nothing and is example ore

---
+++ Content/Tiles/Natural/CoalOre.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace Techarria.Content.Tiles.Natural
+{
+    public class CoalOre : ModTile
+    {
+		public override void SetStaticDefaults()
+		{
+			TileID.Sets.Ore[Type] = true;
+			Main.tileSpelunker[Type] = true; // The tile will be affected by spelunker highlighting
+			Main.tileOreFinderPriority[Type] = 410; // Metal Detector value, see https://terraria.gamepedia.com/Metal_Detector
+			Main.tileShine2[Type] = true; // Modifies the draw color slightly.
+			Main.tileShine[Type] = 975; // How often tiny dust appear off this tile. Larger is less frequently
+			Main.tileMergeDirt[Type] = true;
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+
+			ModTranslation name = CreateMapEntryName();
+			name.SetDefault("Coal Ore");
+			AddMapEntry(new Color(255, 0, 255), name);
+
+			DustType = 84;
+			//ItemDrop = ModContent.ItemType<Items.Placeable.IndustrialCoal>();
+			HitSound = SoundID.Tink;
+			// MineResist = 4f;
+			// MinPick = 200;
+		}
+	}
+}
