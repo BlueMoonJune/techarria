c767a10fb9775fb5073f94299e6a83932e5b1b62
Gwabba <gwabbawaba@gmail.com>
2023-01-26 18:25:42 -0800

    Made infinity crate "obtainable" in "normal" gameplay

--- Content/Items/Placeables/InfinityCrate.cs
+++ Content/Items/Placeables/InfinityCrate.cs
@@ -2,6 +2,7 @@
 using Terraria.GameContent.Creative;
 using Terraria.ModLoader;
 using Terraria;
+using Terraria.Localization;
 
 namespace Techarria.Content.Items.Placeables
 {
@@ -33,12 +34,40 @@ namespace Techarria.Content.Items.Placeables
 		}
 		public override void AddRecipes()
 		{
+			// recipe.AddCondition(NetworkText.FromKey("\n"), r => true); is a new line in the recipe
+
 			Recipe recipe = Recipe.Create(ModContent.ItemType<Placeables.InfinityCrate>());
+			// requires various tiles
 			recipe.AddTile(TileID.Heart);
 			recipe.AddTile(TileID.DemonAltar);
-			recipe.AddCondition(Recipe.Condition.InSkyHeight);
-			recipe.AddIngredient(ItemID.GoldWaterStrider, 89911);
-			recipe.AddIngredient(ItemID.Fake_newchest1);
+			// requires water candle buff
+			recipe.AddCondition(Recipe.Condition.InWaterCandle);
+			// requires expert mode
+			recipe.AddCondition(NetworkText.FromKey("\n"), r => true);
+			recipe.AddCondition(NetworkText.FromKey("No casuals"), r => Main.masterMode == true);
+			// npc conditions
+			recipe.AddCondition(NetworkText.FromKey("\n"), r => true);
+			recipe.AddCondition(NetworkText.FromKey("You have talked to Angler"), r => NPC.savedAngler == true);
+			recipe.AddCondition(NetworkText.FromKey("Goblin Tinkerer has not been saved"), r => NPC.savedGoblin == false);
+			// active NPC
+			recipe.AddCondition(NetworkText.FromKey("\n"), r => true);
+			recipe.AddCondition(NetworkText.FromKey("Currently fighting EOL"), r => NPC.AnyNPCs(NPCID.HallowBoss));
+			// boss downed conditions
+			recipe.AddCondition(NetworkText.FromKey("The evil presence has been left alive"), r => NPC.downedBoss1 == false);
+			recipe.AddCondition(NetworkText.FromKey("The Old Man is cursed"), r => NPC.downedBoss3 == false);
+			recipe.AddCondition(NetworkText.FromKey("Golem has not been destroyed"), r => NPC.downedGolemBoss == false);
+			// time conditions
+			recipe.AddCondition(NetworkText.FromKey("\n"), r => true);
+			recipe.AddCondition(NetworkText.FromKey("Daytime"), r => Main.dayTime == true);
+			// player conditions
+			recipe.AddCondition(NetworkText.FromKey("\n"), r => true);
+			recipe.AddCondition(NetworkText.FromKey("100 hp"), r => Main.LocalPlayer.statLife < 100);
+			recipe.AddCondition(NetworkText.FromKey("100 max hp"), r => Main.LocalPlayer.statLifeMax == 100);
+
+			// items required
+			recipe.AddIngredient(ItemID.GoldWaterStrider, 3);
+			recipe.AddIngredient(ItemID.Waldo);
+			// register
 			recipe.Register();
 		}
 	}
