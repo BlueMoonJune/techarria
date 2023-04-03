514424c24afb3d93ba8ff2b8219af03faf39ef06
Gwabba <gwabbawaba@gmail.com>
2023-04-02 23:35:25 -0700

    grenhius

--- Content/Tiles/GreenhouseGlass.cs
+++ Content/Tiles/GreenhouseGlass.cs
@@ -30,4 +30,32 @@ namespace Techarria.Content.Tiles
 			HitSound = SoundID.Tink;
 		}
 	}
+
+	public class GreenhouseAccentGlass : GreenhouseGlass
+    {
+		public override void SetStaticDefaults()
+		{
+			for (int i = 0; i < Main.tileMerge.Length; i++)
+			{
+				if (Main.tileMerge[i][1])
+				{
+					Main.tileMerge[i][Type] = true;
+				}
+			}
+
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = false;
+
+			// blends like stone
+			//Main.tileBrick[Type] = true;
+
+			// map stuff
+			LocalizedText name = CreateMapEntryName();
+			AddMapEntry(new Color(254, 208, 200), name);
+
+			DustType = 84;
+			ItemDrop = ModContent.ItemType<Items.Placeables.GreenhouseAccentGlass>();
+			HitSound = SoundID.Tink;
+		}
+	}
 }
